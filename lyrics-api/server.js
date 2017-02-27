3e8877c64caa2021a719087ebdac5a26b9c52b8c
ariel.duarte <ariel.duarte@cision.com>
2017-02-27 18:14:55 -0300

    Route config on express server

--- lyrics-api/server.js
+++ lyrics-api/server.js
@@ -8,16 +8,21 @@ var port = process.env.PORT || 3000;
 
 // Always use express inbuilt router.
 var router = express.Router();
-router.get('/',function(req,res){
- // Express determines the common header type.
- res.end("<h1>Express and Nodemon is running now</h1>");
+router.get('/:isAlive?', (req, res) => {
+ //Send HTML a message
+ /*res.end("<h1>Express and Nodemon is running now</h1>");*/
+ var param = req.params.isAlive;
+ //send json a message
+ if(req.params.isAlive){
+ 	res.status(200).send({serverConnection: "is alive!!!"});
+ }else{
+ 	res.end("<h1>Welcome to LyricsT app 1.0</h1>");
+ }
+ 
 });
 // This will navigate all router to proceed /home
 app.use('/home',router);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.listen(port, function(){
-	console.log(`Listening at Server http://localhost:${port}`);
-});
-
+app.listen(port, () => console.log(`Listening at Server http://localhost:${port}`));
