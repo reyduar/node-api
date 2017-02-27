6f2380dbe679d38414a620e4743989bde4231c85
ariel.duarte <ariel.duarte@cision.com>
2017-02-27 16:02:37 -0300

    config express.js and body-parser

--- lyrics-api/server.js
+++ lyrics-api/server.js
@@ -1,6 +1,11 @@
 // load express module.
 var express = require("express");
+var bodyParser = require('body-parser')
+
+
 var app = express();
+var port = process.env.PORT || 3000;
+
 // Always use express inbuilt router.
 var router = express.Router();
 router.get('/',function(req,res){
@@ -9,5 +14,10 @@ router.get('/',function(req,res){
 });
 // This will navigate all router to proceed /home
 app.use('/home',router);
-app.listen(3000);
-console.log("Listening at Port 3000");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(port, function(){
+	console.log(`Listening at Server http://localhost:${port}`);
+});
+
