59600516f2c18bc8083c1cf88b69e8aeb08a78ff
ariel.duarte <arielpy@gmail.com>
2017-02-28 03:26:27 -0300

    Added models and CRUD with mongoose for lyrics collection

--- lyrics-api/controllers/lyricsController.js
+++ lyrics-api/controllers/lyricsController.js
@@ -1,33 +1,85 @@
 'use stritc'
 
+var Lyric = require('../models/lyric');
 function isAlive (req, res) {
 	res.status(200).send({serverConnection: "is alive!!!"});
 }
 
 function addLyric (req, res){
 	var params = req.body;
-	res.status(200).send({body: params, method: "POST"});
+	var lyric = new Lyric();
+	lyric.name = params.name;
+	lyric.artist = params.artist;
+	lyric.album = params.album;
+	lyric.year = params.year;
+	lyric.country = params.country;
+	lyric.lyricOrig = params.lyricOrig;
+	lyric.lyricTrans = params.lyricTrans;
+	lyric.save((err, lyricStored) => {
+		if(err){
+			res.status(500).send({message: "Error to add new lyric"});
+		}else{
+			res.status(200).send({message: "Success to add new lyric", lyric: lyricStored});
+		}
+	});
 }
 
 function editLyrics (req, res){
 	var id = req.params.id;
 	var params = req.body;
-	res.status(200).send({lyricId: id, body: params, method: "PUT"});
+	Lyric.findByIdAndUpdate(id, params, (err, lyricUpdated) => {
+		if(err){
+			res.status(500).send({message: "Error to update lyric", lyricId: id});
+		}
+		
+		res.status(200).send({message: "Success to update lyric", lyric: lyricUpdated});
+	});
 }
 
 function deleteLyrics (req, res){
 	var id = req.params.id;
-	res.status(200).send({lyricId: id, method: "DETELE"});
+	Lyric.findById(id, (err, lyric) => {
+		if(err){
+			res.status(500).send({message: "Error to get lyric", lyricId: id});
+		}
+		if(!lyric){
+			res.status(404).send({message: "No found"});
+		}else{
+			lyric.remove(err => {
+				if(err){
+					res.status(500).send({message: "Error to delete lyric", lyricId: id});
+				}else{
+					res.status(200).send({message: "Success to delete lyric", lyric: lyric});
+				}
+			});
+		}
+	});
 	
 }
 
 function getAllLyrics (req, res){
-	res.status(200).send({data: "lyrics list", method: "GET"});
+	Lyric.find({}).sort('artist').exec((err, lyrics) => {
+		if(err){
+			res.status(500).send({message: "Error to get all lyrics"});
+		}
+		if(!lyrics){
+			res.status(404).send({message: "Empty lyrics collection"});
+		}
+		res.status(200).send({lyrics: lyrics});
+	});
 }
 
 function getLyricById (req, res){
 	var id = req.params.id;
-	res.status(200).send({lyricId: id, method: "GET"});
+	Lyric.findById(id, (err, lyric) => {
+		if(err){
+			res.status(500).send({message: "Error to get lyric", lyricId: id});
+		}
+		if(!lyric){
+			res.status(404).send({message: "No found"});
+		}
+		res.status(200).send({lyric: lyric});
+	});
 }
 
 
